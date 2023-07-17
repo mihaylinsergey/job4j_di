96ae741b2a93e3a273b73927426c2ae14c4e118f
mihaylinsergey <semi999@mail.ru>
2023-07-17 11:17:18 +0300

    0. Что такое DI? [#6876]

---
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -0,0 +1,27 @@
+package ru.job4j.di;
+
+import java.util.List;
+
+public class StartUI {
+    private Store store;
+    private ConsoleInput consoleInput;
+
+    public StartUI(Store store, ConsoleInput consoleInput) {
+        this.store = store;
+        this.consoleInput = consoleInput;
+    }
+
+    public void add(String value) {
+        store.add(value);
+    }
+
+    public void print() {
+        for (String value : store.getAll()) {
+            System.out.println(value);
+        }
+    }
+
+    public void consoleDate(String question) {
+        store.add(consoleInput.askStr(question));
+        }
+}
