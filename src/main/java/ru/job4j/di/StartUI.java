243d0a3e3d54aef7df0f5eb4543f0d304138421f
mihaylinsergey <semi999@mail.ru>
2023-07-17 13:46:48 +0300

    2. Сканирование проекта [#1070]

--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -1,7 +1,8 @@
 package ru.job4j.di;
 
-import java.util.List;
+import org.springframework.stereotype.Component;
 
+@Component
 public class StartUI {
     private Store store;
     private ConsoleInput consoleInput;
